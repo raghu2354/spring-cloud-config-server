61a2603969d57a5cbdd7f086a71ecb3bdbb1527b
raghu2354 <hkrram0192@gmail.com>
2019-09-23 01:28:56 +0530

    cloud-config-server commit

---
+++ spring-config-server/src/main/java/com/microservices/cloud/springconfigserver/SpringConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.microservices.cloud.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+}
